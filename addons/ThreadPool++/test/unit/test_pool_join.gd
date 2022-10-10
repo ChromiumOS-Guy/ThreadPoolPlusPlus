d92ed04183ec300c95a187cff9700438bab98725
ChromiumOS-Guy <57168079+ChromiumOS-Guy@users.noreply.github.com>
2022-10-10 17:57:35 +0300

    Add files via upload

--- addons/ThreadPool++/test/unit/test_pool_join.gd
+++ addons/ThreadPool++/test/unit/test_pool_join.gd
@@ -9,22 +9,23 @@ func before_each():
 	pool.__pool = pool.__create_pool()
 
 func test_pool_join():
-	var result = pool.submit_task_as_only_parameter(self, "join_nothing", "null", 0, 0)
+	var result = pool.submit_task_as_only_parameter(self, "join_nothing", "test", 0, 0)
+	var result2 = pool.submit_task_as_only_parameter(self, "join_nothing", "test2", 0, 0)
 	var tick = OS.get_ticks_msec()
 	while OS.get_ticks_msec() - tick < 10000:
-		if result != null:
+		if result != null and result2 != null:
 			break
 		OS.delay_msec(1)
 	tick = OS.get_ticks_msec()
 	while OS.get_ticks_msec() - tick < 4000:
-		if result.result != null:
+		if result.result != null and result2.result != null:
 			break
 		OS.delay_msec(1)
-	assert_true(result.result)
+	assert_true(true if result.result == null and result2.result == null else false)
 	
 
 func __do_nothing():
-	OS.delay_msec(2000)
+	OS.delay_msec(20000)
 
 func join_nothing(task):
 	OS.delay_msec(1000)
